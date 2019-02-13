3ccd3f575d59d78abeeff5dd12c8e7920bc93b82
Lukas Wilén <bitterbutterbro@gmail.com>
2019-02-13 13:54:35 +0100

    FirstPersonRaycast Script 13/2

---
+++ Hilberts Hotel/Assets/Scripts/Scripts-W/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Public variables
+    public float walkSpeed;
+
+    //Private variables
+    Rigidbody rb;
+    Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    //Non-Physics steps
+    void Update()
+    {
+
+       //GetType directional input from the user
+       float horizontalMovement = Input.GetAxisRaw("Horizontal");
+       float verticalMovement = Input.GetAxisRaw("Vertical");
+
+
+       moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+    }
+
+    //Physics steps
+    void FixedUpdate()
+    {
+        //Call the Move function
+        Move();
+    }
+
+    void Move()
+    {
+
+    }
+}

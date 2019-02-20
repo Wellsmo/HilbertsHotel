d20c4100475bf6851038e60ee04107377545e800
Leon <leon.svensson98@gmail.com>
2019-02-20 13:51:18 +0100

    InputField
    Gjorde ett fungerande InputField

--- Hilberts Hotel/Assets/Scripts/Scripts-Leon/SwitchScript.cs
+++ Hilberts Hotel/Assets/Scripts/Scripts-Leon/SwitchScript.cs
@@ -10,7 +10,7 @@ public class SwitchScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //cOnTriggerEnter();  
+        //OnTriggerEnter();  
     }
 
     // Update is called once per frame

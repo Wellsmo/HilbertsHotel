7e6db4f70d720ea9b49f5dcd3643eb8ad30f0071
Lukas Wilén <bitterbutterbro@gmail.com>
2019-03-11 12:37:30 +0100

    Puzzle Functions
    Fixar funktioner för pussel.

---
+++ Hilberts Hotel/Assets/Scripts/Scripts-W/CoinScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+	void Update () {
+		ReactivateChild ();
+
+	}
+	void ReactivateChild(){
+		foreach (Transform child in this.transform) {
+			if (Input.GetKey (KeyCode.R)) {
+				child.gameObject.SetActive (true);
+			}
+		}
+	}
+}
+
---
+++ Hilberts Hotel/Assets/Scripts/Scripts-W/KeySnapGuide.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySnapGuide : MonoBehaviour
+{
+
+    Vector3 keysnapPos;
+
+    public GameObject tempParent;
+    public GameObject key;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Keyhole"))
+        {
+            key.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            key.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            key.transform.SetParent(tempParent.transform);
+
+            key.transform.position = keysnapPos;
+        }
+    }
+}

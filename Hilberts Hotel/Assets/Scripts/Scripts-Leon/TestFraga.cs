88e86cb45b4a6f31734758a95410ca991235b4c0
Leon <leon.svensson98@gmail.com>
2019-03-18 13:53:59 +0100

    Input field
    Du kan nu stänga lampor av och på och stänga och öppna en dörr

--- Hilberts Hotel/Assets/Scripts/Scripts-Leon/TestFraga.cs
+++ Hilberts Hotel/Assets/Scripts/Scripts-Leon/TestFraga.cs
@@ -9,6 +9,7 @@ public class TestFraga : MonoBehaviour
     public Text Svar;
     public InputField Textfält;
     public GameObject door;
+    public GameObject redLight;
 
     // Start is called before the first frame update
     void Start()
@@ -19,45 +20,39 @@ public class TestFraga : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetKeyDown("return"))
-        {
-            //if (Textfält.text == "Öppna")
-            if ((transform != null) && (Textfält.text == "Öppna"))
-            {
-                SetActiveAllChildren(Transform transform, bool value);
-                Svar.text = "Sweet";
-                              
-
-            }
-        }*/
         SetActiveAllChildren();
     }
 
 
-    //Gör så att childobjecten stängs av
+    //Gör så att gameobject "door" stängs av
     private void SetActiveAllChildren()
     {
-        foreach (Transform child in this.transform)
-        {
+        //foreach (Transform child in this.transform)
+        //{
             if (Input.GetKeyDown("return") && Textfält.text == "Open")
             {
-               door.gameObject.SetActive(false);
+                door.gameObject.SetActive(false);
                 //SetActiveAllChildren(child, false);
                 Svar.text = "Sweet";
+            }
 
-                if (Textfält.text == "Open")
-                {
-
-                    /*
-
-                    */
-                                      
-                }
-
+            if (Input.GetKeyDown("return") && Textfält.text == "Close")
+            {
+                door.gameObject.SetActive(true);
 
             }
 
-        }
+             if (Input.GetKeyDown("return") && Textfält.text == "Lights Off")
+            {
+                redLight.gameObject.SetActive(false);
+
+            }
+             if (Input.GetKeyDown("return") && Textfält.text == "Lights On")
+            {
+                redLight.gameObject.SetActive(true);
+
+            }
+        //}
     }
 
     
